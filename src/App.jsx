a81a6965965f47e841b6240935707297fbae70dd
SumiAkter2 <akteralisha36@gmail.com>
2025-05-26 10:25:16 +0600

    adde header

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Banner from "./Components/Banner";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,22 @@
+import coinImg from "../assets/imgs/coin.png";
+import logoImg from "../assets/imgs/logo-brand.png";
+const Header = () => {
+  return (
+    <div className="mx-12">
+      <div className="flex  justify-between items-center">
+        <a href="/">
+          <img src={logoImg} alt="logo-image" />
+        </a>
+        <div className="flex justify-between items-center gap-x-6">
+          <a href="/">Home</a>
+          <a href="/">Teams</a>
+          <button className="flex justify-center items-center gap-x-1 py-2 px-4 border border-gray-200 rounded-md font-bold">
+            0 Coins <img src={coinImg} alt="coin-img" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
